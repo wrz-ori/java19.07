521cc776fa19aabd6fbdf3b53c9c46cfda0b4057
lisi <ssss@qq.com>
2019-12-10 15:16:41 +0800

    this is a test,please ignron it

---
+++ src/main/java/com/springboot/SpringmybatisApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmybatisApplication.class, args);
+	}
+
+}
